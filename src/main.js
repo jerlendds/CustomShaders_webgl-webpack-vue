d92d7768c15b58c6fdcfa2d292620e11f58e763d
jerlendds <jerlends_0x0@protonmail.com>
2021-03-18 11:48:04 -0600

    formatting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-// ! NTS: DO NOT INCLUDE COMMENTED CODE IN ARTICLE POST
-// import router from './router'
-// import store from './store'
+
 Vue.config.productionTip = false;
 
 new Vue({
